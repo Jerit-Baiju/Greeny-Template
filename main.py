0ee1dc1d1ba4601f3a06a3908a7c2b0f0b4d9d5d
Jerit Baiju <jeritalumkal@gmail.com>
2022-08-06 20:32:54 +0530

    .

--- main.py
+++
@@ -1,8 +0,0 @@
-from pyflit import Page
-
-while True:
-    if open('pages/test.html','r').read() != open('templates/out.html','r').read():
-        main = Page('main')
-        main.add_page('test.html')
-        open('templates/out.html','w').write(open('pages/test.html','r').read())
-        print("UPDATED")
