87822b8dc56092716882675470133725d2d1b5e7
Jerit Baiju <jeritalumkal@gmail.com>
2022-08-06 19:57:49 +0530

    .

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-from flask import Flask
-from liveserver import LiveServer
 from pyflit import Page
 
-app = Flask(__name__)
-ls = LiveServer(app)
-
-@app.route('/')
-def index():
-    return ls.render_template('index.html')
-
-@app.route('/test')
-def test():
-    page = Page('index')
-    return ls.render_template(page.export())
-
-
-ls.run("192.168.43.21",7000, debug=True)
+while True:
+    if open('pages/test.html','r').read() != open('templates/out.html','r').read():
+        main = Page('main')
+        main.add_page('test.html')
+        open('templates/out.html','w').write(open('pages/test.html','r').read())
+        print("UPDATED")
